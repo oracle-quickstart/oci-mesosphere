18ace081c450647892e35eed384b3a5f21f016d0
Torsten Boettjer <31235842+tboettje@users.noreply.github.com>
2019-03-20 07:52:21 +0100

    Creating vcn.tf
    Test credentials via vcn creation

--- vcn.tf
+++ vcn.tf
@@ -6,7 +6,7 @@ variable "compartment_ocid" {}
 variable "region" {}
 
 provider "oci" {
-  version          = ">= 3.0.0"
+  version          = ">= 3.18.0"
   tenancy_ocid     = "${var.tenancy_ocid}"
   user_ocid        = "${var.user_ocid}"
   fingerprint      = "${var.fingerprint}"
@@ -16,7 +16,7 @@ provider "oci" {
 
 resource "oci_core_virtual_network" "simple-vcn" {
   cidr_block     = "10.1.0.0/16"
-  dns_label      = "vcn1"
+  dns_label      = "vcntb1"
   compartment_id = "${var.compartment_ocid}"
-  display_name   = "simple-vcn"
+  display_name   = "service-vcn"
 }
