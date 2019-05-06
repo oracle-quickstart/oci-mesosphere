bb00a89e246aecd52de0e9833fdcf2a8b5a273aa
Torsten <tboettjer@outlook.com>
2019-05-06 02:17:57 +0200

    Update diskmount.sh

--- modules/packer/scripts/diskmount.sh
+++ modules/packer/scripts/diskmount.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Started diskmount script" | sudo tee -a /var/log/diskmount.log
+echo "Attaching disk" | sudo tee -a /var/log/diskmount.log
 counter=0
 while [ ! -e /dev/oracleoci/oraclevdb ]; do
     sleep 100
@@ -9,11 +9,11 @@ while [ ! -e /dev/oracleoci/oraclevdb ]; do
       exit
     fi
 done
-echo "Attaching '/dev/oracleoci/oraclevdb' succeeded" | sudo tee -a /var/log/diskmount.log
+echo "Attached disk '/dev/oracleoci/oraclevdb' " | sudo tee -a /var/log/diskmount.log
 (echo g; echo n; echo ''; echo ''; echo ''; echo w) | sudo fdisk /dev/oracleoci/oraclevdb
 while [[ ! -e /dev/oracleoci/oraclevdb1 ]]; do sleep 5; done
 sudo /sbin/mkfs.ext3 /dev/oracleoci/oraclevdb1
 sudo mkdir -p /var/lib/dcos
 echo '/dev/oracleoci/oraclevdb1 /var/lib/dcos ext3 defaults,_netdev,noatime 0 2' | sudo tee -a /etc/fstab
 sudo mount -a
-echo "Mounting '/var/lib/dcos' succeeded" | sudo tee -a /var/log/diskmount.log
+echo "Mounted '/dev/oracleoci/oraclevdb' at'/var/lib/dcos' " | sudo tee -a /var/log/diskmount.log
